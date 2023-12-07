73ddfe84ee6981399f72e6582da1d4d491f57879
Mike Sheldon <m.sheldon@tools4ever.com>
2023-12-07 07:34:07 -0900

    Create IBM DB2 Queries.ps1

---
+++ IBM DB2 Queries.ps1
@@ -0,0 +1,629 @@
+$Log_MaskableKeys = @(
+    'password'
+)
+
+
+#
+# System functions
+#
+
+function Idm-SystemInfo {
+    param (
+        # Operations
+        [switch] $Connection,
+        [switch] $TestConnection,
+        [switch] $Configuration,
+        # Parameters
+        [string] $ConnectionParams
+    )
+
+    Log info "-Connection=$Connection -TestConnection=$TestConnection -Configuration=$Configuration -ConnectionParams='$ConnectionParams'"
+    
+    if ($Connection) {
+        @(            
+            @{
+                name = 'driver_name'
+                type = 'textbox'
+                label = 'Driver Name'
+                description = 'Name of Driver found in ODBC Admin'
+                value = 'IBM DB2 ODBC DRIVER - C_PROGRA~1_IBM'
+            }
+            @{
+                name = 'host_name'
+                type = 'textbox'
+                label = 'Server'
+                description = 'IP or Hostname of Server'
+                value = ''
+            }
+            @{
+                name = 'port'
+                type = 'textbox'
+                label = 'Port'
+                description = 'Instance port'
+                value = '50000'
+            }
+            @{
+                name = 'database'
+                type = 'textbox'
+                label = 'Database'
+                description = 'Name of database'
+                value = 'PDSELITX'
+            }
+			@{
+                name = 'schema'
+                type = 'textbox'
+                label = 'Database'
+                description = 'Name of schema'
+                value = 'TEAMS'
+            }
+            @{
+                name = 'user'
+                type = 'textbox'
+                label = 'Username'
+                label_indent = $true
+                description = 'User account name to access server'
+            }
+            @{
+                name = 'password'
+                type = 'textbox'
+                password = $true
+                label = 'Password'
+                label_indent = $true
+                description = 'User account password to access server'
+            }
+            @{
+                name = 'timeout'
+                type = 'textbox'
+                label = 'Timeout'
+                description = 'O = no timeou'
+                value = '0'
+            }
+            @{
+                name = 'table_1_name'
+                type = 'textbox'
+                label = 'Query 1 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_1_query'
+                type = 'textbox'
+                label = 'Query 1 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_2_name'
+                type = 'textbox'
+                label = 'Query 2 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_2_query'
+                type = 'textbox'
+                label = 'Query 2 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_3_name'
+                type = 'textbox'
+                label = 'Query 3 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_3_query'
+                type = 'textbox'
+                label = 'Query 3 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_4_name'
+                type = 'textbox'
+                label = 'Query 4 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_4_query'
+                type = 'textbox'
+                label = 'Query 4 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_5_name'
+                type = 'textbox'
+                label = 'Query 5 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_5_query'
+                type = 'textbox'
+                label = 'Query 5 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_6_name'
+                type = 'textbox'
+                label = 'Query 6 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_6_query'
+                type = 'textbox'
+                label = 'Query 6 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_7_name'
+                type = 'textbox'
+                label = 'Query 7 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_7_query'
+                type = 'textbox'
+                label = 'Query 7 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_8_name'
+                type = 'textbox'
+                label = 'Query 8 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_8_query'
+                type = 'textbox'
+                label = 'Query 8 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_9_name'
+                type = 'textbox'
+                label = 'Query 9 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_9_query'
+                type = 'textbox'
+                label = 'Query 9 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_10_name'
+                type = 'textbox'
+                label = 'Query 10 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_10_query'
+                type = 'textbox'
+                label = 'Query 10 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_11_name'
+                type = 'textbox'
+                label = 'Query 11 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_11_query'
+                type = 'textbox'
+                label = 'Query 11 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_12_name'
+                type = 'textbox'
+                label = 'Query 12 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_12_query'
+                type = 'textbox'
+                label = 'Query 12 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_13_name'
+                type = 'textbox'
+                label = 'Query 13 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_13_query'
+                type = 'textbox'
+                label = 'Query 13 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_14_name'
+                type = 'textbox'
+                label = 'Query 14 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_14_query'
+                type = 'textbox'
+                label = 'Query 14 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_15_name'
+                type = 'textbox'
+                label = 'Query 15 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_15_query'
+                type = 'textbox'
+                label = 'Query 15 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_16_name'
+                type = 'textbox'
+                label = 'Query 16 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_16_query'
+                type = 'textbox'
+                label = 'Query 16 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_17_name'
+                type = 'textbox'
+                label = 'Query 17 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_17_query'
+                type = 'textbox'
+                label = 'Query 17 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_18_name'
+                type = 'textbox'
+                label = 'Query 18 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_18_query'
+                type = 'textbox'
+                label = 'Query 18 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_19_name'
+                type = 'textbox'
+                label = 'Query 19 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_19_query'
+                type = 'textbox'
+                label = 'Query 19 - SQL Statement'
+                description = ''
+            }
+            @{
+                name = 'table_20_name'
+                type = 'textbox'
+                label = 'Query 20 - Name of Table'
+                description = ''
+            }
+            @{
+                name = 'table_20_query'
+                type = 'textbox'
+                label = 'Query 20 - SQL Statement'
+                description = ''
+            }
+        )
+    }
+
+    if ($TestConnection) {
+        Open-DB2Connection $ConnectionParams
+    }
+
+    if ($Configuration) {
+        @()
+    }
+
+    Log info "Done"
+}
+
+
+function Idm-OnUnload {
+    Close-DB2Connection
+}
+
+
+#
+# CRUD functions
+#
+
+$ColumnsInfoCache = @{}
+
+
+function Idm-Dispatcher {
+    param (
+        # Optional Class/Operation
+        [string] $Class,
+        [string] $Operation,
+        # Mode
+        [switch] $GetMeta,
+        # Parameters
+        [string] $SystemParams,
+        [string] $FunctionParams
+    )
+
+    Log info "-Class='$Class' -Operation='$Operation' -GetMeta=$GetMeta -SystemParams='$SystemParams' -FunctionParams='$FunctionParams'"
+    $connection_params = ConvertFrom-Json2 $SystemParams
+
+    if ($Class -eq '') {
+
+        if ($GetMeta) {
+            #
+            # Get all tables and views in database
+            #
+
+            Open-DB2Connection $SystemParams
+
+            #
+            # Output list of supported operations per table/view (named Class)
+            #
+            for ($i = 0; $i -lt 21; $i++)
+            {
+                if($connection_params."table_$($i)_name".length -gt 0)
+                {
+                    @(
+                        [ordered]@{
+                            Class = $connection_params."table_$($i)_name"
+                            Operation = 'Read'
+                            'Source type' = 'Query'
+                            'Primary key' = ''
+                            'Supported operations' = 'R'
+                            'Query' = $connection_params."table_$($i)_query"
+                        }
+                    )
+                    }
+            }
+
+        }
+        else {
+            # Purposely no-operation.
+        }
+
+    }
+    else {
+
+        if ($GetMeta) {
+            #
+            # Get meta data
+            #
+
+            Open-DB2Connection $SystemParams
+
+            @()
+
+        }
+        else {
+            #
+            # Execute function
+            #
+
+            Open-DB2Connection $SystemParams
+
+            for ($i = 0; $i -lt 21; $i++)
+            {
+                if($connection_params."table_$($i)_name" -eq $class)
+                {
+                    $class_query = $connection_params."table_$($i)_query"
+                    break
+                }
+            }
+
+           # $column_query = $class_query.replace("SELECT ","SELECT TOP 5 ")
+		   $column_query = $class_query + " FETCH FIRST 5 ROWS ONLY"
+        
+            $columns = Fill-SqlInfoCache -Query $column_query -Timeout $SystemParams.timeout
+        
+            $Global:ColumnsInfoCache[$Class] = @{
+                primary_keys = @($columns | Where-Object { $_.is_primary_key } | ForEach-Object { $_.name })
+                identity_col = @($columns | Where-Object { $_.is_identity    } | ForEach-Object { $_.name })[0]
+            }
+
+            $primary_keys = $Global:ColumnsInfoCache[$Class].primary_keys
+            $identity_col = $Global:ColumnsInfoCache[$Class].identity_col
+
+            $function_params = ConvertFrom-Json2 $FunctionParams
+
+            # Replace $null by [System.DBNull]::Value
+            $keys_with_null_value = @()
+            foreach ($key in $function_params.Keys) { if ($function_params[$key] -eq $null) { $keys_with_null_value += $key } }
+            foreach ($key in $keys_with_null_value) { $function_params[$key] = [System.DBNull]::Value }
+            
+            $sql_command = New-DB2Command $class_query
+            $sql_command.CommandTimeout = $SystemParams.timeout
+            Invoke-DB2Command $sql_command
+            Dispose-DB2Command $sql_command
+
+        }
+
+    }
+
+    Log info "Done"
+}
+
+
+#
+# Helper functions
+#
+
+function Fill-SqlInfoCache {
+    param (
+        [switch] $Force,
+        [string] $Query,
+        [string] $Class,
+        [string] $Timeout
+    )
+
+    # Refresh cache
+    $sql_command = New-DB2Command $Query
+    $sql_command.CommandTimeout = $Timeout
+    $result = (Invoke-DB2Command $sql_command) | Get-Member -MemberType Properties | Select-Object Name
+    
+    Dispose-DB2Command $sql_command
+
+    $objects = New-Object System.Collections.ArrayList
+    $object = @{}
+    # Process in one pass
+    foreach ($row in $result) {
+            $object = @{
+                full_name = $Class
+                type      = 'Query'
+                columns   = New-Object System.Collections.ArrayList
+            }
+
+        $object.columns.Add(@{
+            name           = $row.Name
+            is_primary_key = $false
+            is_identity    = $false
+            is_computed    = $false
+            is_nullable    = $true
+        }) | Out-Null
+    }
+
+    if ($object.full_name -ne $null) {
+        $objects.Add($object) | Out-Null
+    }
+    @($objects)
+}
+
+function New-DB2Command {
+    param (
+        [string] $CommandText
+    )
+
+    $sql_command = New-Object System.Data.Odbc.OdbcCommand($CommandText, $Global:DB2Connection)
+
+    return $sql_command
+}
+
+
+function Dispose-DB2Command {
+    param (
+        [System.Data.Odbc.OdbcCommand] $SqlCommand
+    )
+
+    $SqlCommand.Dispose()
+}
+
+function Invoke-DB2Command {
+    param (
+        [System.Data.Odbc.OdbcCommand] $SqlCommand
+    )
+
+    function Invoke-DB2Command-ExecuteReader {
+        param (
+            [System.Data.Odbc.OdbcCommand] $SqlCommand
+        )
+        $data_reader = $SqlCommand.ExecuteReader()
+        $column_names = @($data_reader.GetSchemaTable().ColumnName)
+
+        if ($column_names) {
+
+            $hash_table = [ordered]@{}
+
+            foreach ($column_name in $column_names) {
+                $hash_table[$column_name] = ""
+            }
+
+#           $obj = [PSCustomObject]$hash_table
+            $obj = New-Object -TypeName PSObject -Property $hash_table
+
+            # Read data
+            while ($data_reader.Read()) {
+                foreach ($column_name in $column_names) {
+                    $obj.$column_name = if ($data_reader[$column_name] -is [System.DBNull]) { $null } else { $data_reader[$column_name] }
+                }
+
+                # Output data
+                $obj
+            }
+
+        }
+
+        $data_reader.Close()
+
+    }
+
+    try {
+        Invoke-DB2Command-ExecuteReader $SqlCommand
+    }
+    catch {
+        Log error "Failed: $_"
+        Write-Error $_
+    }
+
+    Log debug "Done"
+
+}
+
+function Open-DB2Connection {
+    param (
+        [string] $ConnectionParams
+    )
+
+    $connection_params = ConvertFrom-Json2 $ConnectionParams
+    $connection_string =  "Driver={$($connection_params.driver_name)};Database=$($connection_params.database);Hostname=$($connection_params.host_name);Port=$($connection_params.port);Protocol=TCPIP;Uid=$($connection_params.user);Pwd=$($connection_params.password);CurrentSchema=$($connection_params.schema);AUTHENTICATION=SERVER"
+    
+    LOG info $connection_string
+    
+    if ($Global:DB2Connection -and $connection_string -ne $Global:DB2ConnectionString) {
+        Log info "DB2Connection connection parameters changed"
+        Close-DB2Connection
+    }
+
+    if ($Global:DB2Connection -and $Global:DB2Connection.State -ne 'Open') {
+        Log warn "DB2Connection State is '$($Global:DB2Connection.State)'"
+        Close-DB2Connection
+    }
+
+    Log info "Opening DB2Connection '$connection_string'"
+
+    try {
+        $connection = (new-object System.Data.Odbc.OdbcConnection);
+        $connection.connectionstring = $connection_string
+        $connection.open();
+
+        $Global:DB2Connection       = $connection
+        $Global:DB2ConnectionString = $connection_string
+
+        $Global:ColumnsInfoCache = @{}
+    }
+    catch {
+        Log warn "Failed: $_"
+        #Write-Error $_
+    }
+
+    Log info "Done"
+    
+}
+
+
+function Close-DB2Connection {
+    if ($Global:DB2Connection) {
+        Log info "Closing DB2Connection"
+
+        try {
+            $Global:DB2Connection.Close()
+            $Global:DB2Connection = $null
+        }
+        catch {
+            # Purposely ignoring errors
+        }
+
+        Log info "Done"
+    }
+}
